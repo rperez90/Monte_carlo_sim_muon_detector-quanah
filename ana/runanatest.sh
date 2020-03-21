ac9efd9ad75a60992286597c2636f097647c36d5
Raul Perez Jr <raulperezjr@MacBook-Pro-2.local>
2020-03-21 17:46:36 -0500

    rm runanarp.sh runanatest.sh hist61_45deg_EX*.root hist_test01.root histtest_ana.root out001.txt

--- ana/runanatest.sh
+++
@@ -1,4 +0,0 @@
-c++  `root-config --cflags` -o muonAnalysis3 sc8muontree.cc muonAnalysis3_rp.cc `root-config --glibs`
-
-./muonAnalysis3 /lustre/work/perez90/hep/MuonSC8/mc/v4/sim/muonTreetest_sim.root hist_muontrial_test.root
-#./muonAnalysis3 muonTree06_45deg_EX1simAIRTANK.root  hist_muontrial_test.root
